04089b59a863b667edf3d316e65906e770246d1f
Wekavit-dev <landrymufundi@gmail.com>
2026-05-11 17:00:02 +0200

    Update API endpoint for IP_ADD

--- src/api/utils/address.js
+++ src/api/utils/address.js
@@ -1,4 +1,3 @@
-// export const IP_ADD = `https://api.wekamoney.kubwacu.com/api/v0`
 export const IP_ADD = `https://wekavit-backend.onrender.com/api/v0`;
 // export const IP_ADD = `http://192.168.8.100:8000/api/v0`;
 // export const IP_ADD_INVESTMENT = `http://192.168.8.100:8000/api/v0`;
